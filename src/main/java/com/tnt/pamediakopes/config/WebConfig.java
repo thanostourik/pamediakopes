08b224000fdf2bf14885f5bedec0a8ac814bb67e
Thanos Tourikas <thanos.tourik@gmail.com>
2020-02-01 01:23:32 +0200

    feat: add auditing to movie entity

--- src/main/java/com/tnt/pamediakopes/config/WebConfig.java
+++ src/main/java/com/tnt/pamediakopes/config/WebConfig.java
@@ -5,12 +5,19 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
 @Configuration
 @EnableAutoConfiguration
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
@@ -31,5 +38,10 @@ public class WebConfig implements WebMvcConfigurer {
 		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
 		bean.setValidationMessageSource(messageSource);
 		return bean;
+	}
+
+	@Bean // Makes ZonedDateTime compatible with auditing fields
+	public DateTimeProvider auditingDateTimeProvider() {
+		return () -> Optional.of(ZonedDateTime.now(ZoneId.of("UTC")));
 	}
 }
--- src/main/java/com/tnt/pamediakopes/models/Movie.java
+++ src/main/java/com/tnt/pamediakopes/models/Movie.java
@@ -1,11 +1,16 @@
 package com.tnt.pamediakopes.models;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.ZonedDateTime;
 
 @Data
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Movie {
 
 	@Id
@@ -23,4 +28,11 @@ public class Movie {
 
 	@Column(name = "description", columnDefinition="TEXT")
 	private String description;
+
+	@CreatedDate
+	@Column(name = "inserted_at", nullable = false, updatable = false)
+	private ZonedDateTime insertedAt;
+
+	@LastModifiedDate
+	private ZonedDateTime updatedAt;
 }

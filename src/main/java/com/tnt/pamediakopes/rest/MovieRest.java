f359e6588ffa4f08f863cc3c8e057dbb50cc7344
Thanos Tourikas <thanos.tourik@gmail.com>
2020-02-01 00:45:57 +0200

    feat: add logging to rest endpoints

--- src/main/java/com/tnt/pamediakopes/rest/MovieRest.java
+++ src/main/java/com/tnt/pamediakopes/rest/MovieRest.java
@@ -4,6 +4,8 @@ import com.tnt.pamediakopes.dto.MovieDto;
 import com.tnt.pamediakopes.models.Movie;
 import com.tnt.pamediakopes.service.MovieService;
 import com.tnt.pamediakopes.utils.TransformMovie;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,8 @@ import java.util.stream.StreamSupport;
 @RequestMapping("/api/movies")
 public class MovieRest {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	private MovieService movieService;
 
 	/**
@@ -34,8 +38,14 @@ public class MovieRest {
 		this.movieService = movieService;
 	}
 
+	/**
+	 *
+	 * @return
+	 */
 	@GetMapping
 	public ResponseEntity<List<MovieDto>> get() {
+		logger.info("Get list of movies");
+
 		List<MovieDto> dtos = StreamSupport.stream(movieService.findAll().spliterator(), false)
 				.map(TransformMovie::toDtoMin)
 				.collect(Collectors.toList());
@@ -43,22 +53,45 @@ public class MovieRest {
 		return ResponseEntity.ok(dtos);
 	}
 
+	/**
+	 *
+	 * @param id
+	 * @return
+	 */
 	@GetMapping("/{id}")
 	public ResponseEntity<MovieDto> get(@PathVariable Long id) {
+		logger.info("Get movie details id: " + id);
+
 		return movieService.findById(id)
 				.map(TransformMovie::toDto)
 				.map(ResponseEntity::ok)
 				.orElse(ResponseEntity.notFound().build());
 	}
 
+	/**
+	 *
+	 * @param dto
+	 * @return
+	 */
 	@PostMapping
 	public ResponseEntity<MovieDto> post(@Valid @RequestBody MovieDto dto) {
+		logger.info("Create new movie");
+
 		Movie movie = TransformMovie.fromDto(dto);
 		movie = movieService.save(movie);
 		MovieDto created = TransformMovie.toDto(movie);
+
+		logger.info("Created movie with id: " + movie.getId());
+
 		return ResponseEntity.ok(created);
 	}
 
+	/**
+	 * Handle validation exceptions
+	 *
+	 * @param ex
+	 * @return
+	 */
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {

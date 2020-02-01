097c95c106d2838dc7eb16d0f6b83a86fae8c980
Thanos Tourikas <thanos.tourik@gmail.com>
2020-02-01 20:39:43 +0200

    fix: fix routing when packaging frontend and backend to one jar

--- src/main/java/com/tnt/pamediakopes/PamediakopesApplication.java
+++ src/main/java/com/tnt/pamediakopes/PamediakopesApplication.java
@@ -4,9 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class PamediakopesApplication extends SpringBootServletInitializer {
+
+	/**
+	 * Used to make SPA's routes work
+	 * Forward to index when a request to non root path occurs
+	 *
+	 * @return
+	 */
+	@RequestMapping(value = "/{path:[^\\.]*}")
+	public String redirect() {
+		return "forward:/";
+	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
